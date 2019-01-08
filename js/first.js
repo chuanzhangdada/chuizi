69320b1ab94b0de71d3acc3302681aaa451f3fb1
haoxinning <954337612@qq.com>
2019-01-08 19:17:56 +0800

    5

--- js/first.js
+++ js/first.js
@@ -1,15 +1,20 @@
 //郝鑫宁   js部分
-window.onload = function(){
-	var oUl = document.querySelector('.top-nav1');
-	var oLi = document.querySelectorAll('.top-nav-item');
-	oLi.onmouseover = function(){
-		oLi.style.backgroundColor = 'white';
-		oLi.style.fontWeight = 'bold';
-	}
-	oLi.onmouseout = function(){
-		oLi.style.backgroundColor = '#666';
-	}
-}
+
+/*
+   处理导航栏部分
+*/
+
+// window.onload = function(){
+// 	var oUl = document.querySelector('.top-nav1');
+// 	var oLi = document.querySelectorAll('.top-nav-item');
+// 	oLi.onmouseover = function(){
+// 		oLi.style.backgroundColor = 'white';
+// 		oLi.style.fontWeight = 'bold';
+// 	}
+// 	oLi.onmouseout = function(){
+// 		oLi.style.backgroundColor = '#666';
+// 	}
+// }
 
 window.onload = function(){
 	// 顶部导航购物车js
